dedceaef9b0d7e6c785be4bbeffccec2158fdc4c
Alexandre <alexandre.venturi@outlook.com>
2023-12-21 11:45:26 +0100

    update GameManager

--- src/Reversi.scala
+++ src/Reversi.scala
@@ -3,7 +3,7 @@ import java.awt.Color
 class Reversi {
   var grid: Array[Array[Coin]] = Array.ofDim(8,8)
   var selected: Coin
-
+  var gm : GameManager = new GameManager(false)
 
   // TODO
   def play(): Unit ={
@@ -15,7 +15,7 @@ class Reversi {
 
   def isOccupied(row: Int, col: Int): Boolean = {
     if (!grid(row)(col).busy){
-
+      return false
     }
     return true
   }
@@ -29,8 +29,19 @@ class Reversi {
   // TODO
 
   def placeCoin(x: Int, y: Int): Unit = {
+    if(!(isOccupied(x,y)) && isLegal()){
+      grid(x)(y).busy = true
+      if (gm.turn) {
+        grid(x)(y).c = Color.white
+      } else
+        grid(x)(y).c = Color.black
+      gm.turn = !gm.turn
+    }
     // TODO
   }
+
+
+  def updateCoins(c : Color,x : Int, y : Int)
 
   //TODO
 
@@ -59,15 +70,38 @@ class Reversi {
     grid(4)(3).c = Color.black
     return tab
   }
+
+  def countWhite(): Int = {
+    var count : Int = 0
+    for (i <- grid.indices){
+      for (j <- grid(i).indices){
+        if (grid(i)(j).c == Color.white){
+          count+=1
+        }
+      }
+    }
+    return count
+  }
+
+  def countBlack(): Int = {
+    var count: Int = 0
+    for (i <- grid.indices) {
+      for (j <- grid(i).indices) {
+        if (grid(i)(j).c == Color.black) {
+          count += 1
+        }
+      }
+    }
+    return count
+  }
+
 }
 
 
 class Coin(var row: Char, var col: Int, var c: Color, var busy: Boolean){
-  // TODO
-
 }
 
-class GameManager {
+class GameManager(var turn : Boolean) {
 
 
   // TODO
@@ -76,12 +110,18 @@ class GameManager {
   }
 
   def askTurn(): Unit = {
-    // TODO
+    // false = noir
+    // true = blanc
+    if (turn)
+      turn = false
+    else
+      turn = true
   }
 
-  def displayscore(): String = {
-    // TODO
-    return "aa"
+
+
+  def displayscore(w : Int, b : Int): String = {
+    return s"$w - $b"
   }
   // TODO
 }
