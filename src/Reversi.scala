685c1803e39d15887ef8e452d18b6610dd8718d4
Ana <anagoncalves7@icloud.com>
2023-12-21 10:45:45 +0100

    update class Reversi with all methods (not complete)

--- src/Reversi.scala
+++ src/Reversi.scala
@@ -1,9 +1,32 @@
 import java.awt.Color
 
 class Reversi {
+  var grid: Array[Array[Coin]] = Array.ofDim(8,8)
 
-  var grid: Array[Array[Int]] = new Array(10)
+  // TODO
+  def play(): Unit ={
 
+  }
+
+  // TODO
+  def isOccupied(): Boolean = {
+    return true
+  }
+
+  // TODO
+  def isLegal(): Boolean = {
+    return true
+  }
+
+  // TODO
+  def placeCoin(x: Int, y: Int): Unit = {
+
+  }
+
+  //TODO
+  def endGame(): Boolean = {
+    return true
+  }
 }
 
 class Coin(c : Color, busy : Boolean){
