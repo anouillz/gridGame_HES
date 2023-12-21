059efbd3f222822634c8ac9846e60326245aa3f2
Ana <anagoncalves7@icloud.com>
2023-12-21 11:24:41 +0100

    update method isOccupied

--- src/Reversi.scala
+++ src/Reversi.scala
@@ -2,26 +2,21 @@ import java.awt.Color
 
 class Reversi {
   var grid: Array[Array[Coin]] = Array.ofDim(8,8)
+  var selected: Coin
 
 
   // TODO
   def play(): Unit ={
     grid = fillGrid(grid)
-    for (i : Int <- 3 to 4){
-      for(j : Int <- 3 to 4){
-        grid(i)(j).busy = true
-      }
-    }
-    grid(3)(3).c = Color.white
-    grid(4)(4).c = Color.white
-    grid(3)(4).c = Color.black
-    grid(4)(3).c = Color.black
+
   }
 
   // TODO
 
-  def isOccupied(): Boolean = {
+  def isOccupied(row: Int, col: Int): Boolean = {
+    if (!grid(row)(col).busy){
 
+    }
     return true
   }
 
@@ -44,24 +39,32 @@ class Reversi {
     return true
   }
 
+
+
   def fillGrid(tab : Array[Array[Coin]]): Array[Array[Coin]] = {
     for(i <- tab.indices){
       for(j <- tab(i).indices){
-        var token: Coin = new Coin
-        token.c = Color.green
+        var token: Coin = new Coin((i + ('A'.toInt)).toChar, j+1, Color.green,false)
         tab(i)(j) = token
-
       }
     }
+    for (i : Int <- 3 to 4){
+      for(j : Int <- 3 to 4){
+        grid(i)(j).busy = true
+      }
+    }
+    grid(3)(3).c = Color.white
+    grid(4)(4).c = Color.white
+    grid(3)(4).c = Color.black
+    grid(4)(3).c = Color.black
     return tab
   }
 }
 
 
-class Coin{
+class Coin(var row: Char, var col: Int, var c: Color, var busy: Boolean){
   // TODO
-  var c : Color = Color.green
-  var busy : Boolean = false
+
 }
 
 class GameManager {
